f2f6c978bf5b102d7818039c48d474551f8ac458
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-04-16 01:41:44 +0530

    bubbleSort() method is done

--- src/BubbleSort/BubbleSort.java
+++ src/BubbleSort/BubbleSort.java
@@ -1,4 +1,41 @@
 package BubbleSort;
 
+import java.util.Scanner;
+
 public class BubbleSort {
+    Scanner sc = new Scanner(System.in);
+
+    public void sort(int[] array) {
+        System.out.println("press 1 to sort the array in ascending order");
+        System.out.println("press 2 to sort the array in desending order");
+        int choice = sc.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (choice == 1) {
+                    ascendingSort(array, j);
+                } else {
+                    desendingSort(array, j);
+                }
+
+            }
+        }
+    }
+
+    private void desendingSort(int[] array, int j) {
+        if (array[j] < array[j + 1]) {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+
+    private void ascendingSort(int[] array, int j) {
+        if (array[j] > array[j + 1]) {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+
+
+    }
 }
