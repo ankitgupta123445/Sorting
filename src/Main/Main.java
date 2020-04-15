0e7f59fe65effc684bd6fbac6513b76492a0d4a1
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-04-16 01:43:29 +0530

    Main class is ready for bubbleSort() method

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,30 @@
 package Main;
 
+import BubbleSort.BubbleSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("howmany elements would you like to in the array?");
+        int noOfElement = scanner.nextInt();
+        int[] array = new int[noOfElement];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println("unsorted array");
+        System.out.println(Arrays.toString(array));
+        System.out.println("press 1  to sort this array from Bubble sort");
+        int ch = scanner.nextInt();
+        switch (ch) {
+            case 1:
+                BubbleSort bubbleSort = new BubbleSort();
+                bubbleSort.sort(array);
+                System.out.println("sorted array by bubble sort");
+                System.out.println(Arrays.toString(array));
+                break;
+        }
+    }
 }
