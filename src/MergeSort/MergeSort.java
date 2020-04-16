dfab24530a57464dea5f22064502b9eb7d39a764
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-04-17 00:30:03 +0530

    Main class is done

---
+++ src/MergeSort/MergeSort.java
@@ -0,0 +1,33 @@
+package MergeSort;
+
+public class MergeSort {
+    public static void sort(int[] array) {
+        int half = array.length / 2;
+        int[] leftArray = new int[half];
+        int[] rightArray = new int[array.length - half];
+        System.arraycopy(array, 0, leftArray, 0, half);
+        System.arraycopy(array, half, rightArray, 0, array.length - half);
+        sort(leftArray);
+        sort(rightArray);
+        merge(array, leftArray, rightArray);
+    }
+
+    private static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < leftArray.length && j < rightArray.length) {
+            if (leftArray[i] < rightArray[j]) {
+                array[k++] = leftArray[i++];
+            } else {
+                array[k++] = rightArray[j++];
+            }
+        }
+        while (i < leftArray.length) {
+            array[k++] = leftArray[i++];
+        }
+        while (j < rightArray.length) {
+            array[k++] = rightArray[j++];
+        }
+    }
+}
